9d98bd56bb18d912eea753d511dfa6266d6dd7c2
geass94 <okoridze@yahoo.com>
2019-03-11 20:19:38 +0400

    CORS

--- src/main/java/ge/idealab/kedi/config/SecurityConfig.java
+++ src/main/java/ge/idealab/kedi/config/SecurityConfig.java
@@ -82,8 +82,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.cors().disable();
         http
                 .cors()
                 .and()
