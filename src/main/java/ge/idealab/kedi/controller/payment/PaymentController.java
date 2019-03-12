7afc5216dec4b549341efa16e0d91a1103cea3a0
geass94 <okoridze@yahoo.com>
2019-03-12 14:25:36 +0400

    paymentz

--- src/main/java/ge/idealab/kedi/controller/payment/PaymentController.java
+++ src/main/java/ge/idealab/kedi/controller/payment/PaymentController.java
@@ -1,7 +1,6 @@
 package ge.idealab.kedi.controller.payment;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.ServletException;
@@ -13,7 +12,7 @@ import java.io.IOException;
 @Controller
 @RequestMapping("/payment")
 public class PaymentController {
-    @RequestMapping("/cart/callback")
+    @RequestMapping("/cartu/callback")
     public void cartuCallBack(HttpServletRequest request,
                               HttpServletResponse response) throws IOException, ServletException {
         System.out.println(request.getContextPath());
